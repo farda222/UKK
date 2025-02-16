de4c8a05c837789e7f374d2cb0a94b784bab5341
afifihsnm <afifihsanke2@gmail.com>
2025-02-16 18:51:44 +0700

    fix(db): fix database migration to work properly

--- database/migrations/2025_01_15_013326_create_customers_table.php
+++ database/migrations/2025_01_15_013326_create_customers_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('no_ktp')->unique();
             $table->string('phone')->nullable();
             $table->enum('role', ['admin', 'customer'])->default('customer');
             $table->timestamps();
